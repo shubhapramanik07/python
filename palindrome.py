8cd66d1883e7d1b6c2e769772b59d134a01f9add
shubhapramanik07 <shubhapramanik07@gmail.com>
2025-08-30 17:23:28 +0530

    added palindrome

--- palindrome.py
+++ palindrome.py
@@ -1,18 +1,22 @@
-num = 1235321
+num = 12357321
 n = num
 num1 = 0
 
 
-def is_palindrome(n):
-    original = n
-    num1 = 0
-    while n > 0:
+# def is_palindrome(n):
+#     original = n
+#     num1 = 0
+while n > 0:
         dig = n % 10
         num1 = (num1 * 10) + dig
         n //= 10
-    if num1 == original:
-        print("palindrome!!")
-    else:
-        print("not palindrome")
+if num1 == num:
+     print("palindrome!!")
+else:
+     print("not palindrome")
      
-is_palindrome(n)
+
+##time_compexity will be O(log10 n)
+
+
+# is_palindrome(n)
