eead224af52ac67e3f4b151b034a35b9482dbe81
shubhapramanik07 <shubhapramanik07@gmail.com>
2025-08-31 00:28:24 +0530

    added factors

---
+++ factors.py
@@ -0,0 +1,22 @@
+#factors_of_10 = [1, 2, 5, 10]
+#factors_of_15 = [1, 3, 5, 15]
+#factors_of_20 = [1, 2, 4, 5, 10, 20]
+#factors_of_25 = [1, 5, 25]
+#factors_of_19 = [1, 19]
+
+#######################
+
+n = 19
+num = n
+factors = []
+
+for i in range(1,num+1):
+    if num % i == 0:
+        factors.append(i)
+
+print("factors_of_",n,"=",factors)
+
+
+#time_complexity: O(n)
+#space_complexity: O(k)
+#where k is the number of factors of n
