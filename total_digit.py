ee317fae02917a8975795e099a986a634a212f50
shubhapramanik07 <shubhapramanik07@gmail.com>
2025-08-30 16:47:32 +0530

    Added basic folder with total_digit.py

--- total_digit.py
+++ total_digit.py
@@ -1,6 +1,8 @@
 from math import *
-n = 12563
+num = 12563
+n = num
+#it's a good habit to place variables into another
 def count_digits(n):
     return floor(log10(n)) + 1 if n > 0 else 1
-
+#to print
 print (count_digits(n))
