11f0b7fc57f0eaa528e6aa38cbd6ab47e4782511
shubhapramanik07 <shubhapramanik07@gmail.com>
2025-08-31 16:59:53 +0530

    added factors

--- factors.py
+++ factors.py
@@ -6,17 +6,46 @@
 
 #######################
 
-n = 19
-num = n
-factors = []
+# n = 19
+# num = n
+# factors = []
 
-for i in range(1,num+1):
-    if num % i == 0:
-        factors.append(i)
+# for i in range(1,num+1):
+#     if num % i == 0:
+#         factors.append(i)
 
-print("factors_of_",n,"=",factors)
+# print("factors_of_",n,"=",factors)
 
 
 #time_complexity: O(n)
 #space_complexity: O(k)
 #where k is the number of factors of n
+
+
+
+#another method to solve the problem.....
+from math import sqrt
+arr = []
+num = 20
+for i in range(1, int(sqrt(num)) + 1):
+    if num % i == 0:
+        arr.append(i)
+        if i != (num // i):
+            arr.append(num // i)
+print("factors_of_", num, "=", sorted(arr))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
