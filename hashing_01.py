6920069cae4931156117b252e5e0c393f9487779
shubhapramanik07 <shubhapramanik07@gmail.com>
2025-09-02 23:37:53 +0530

    hashing01 charecter_hashing

--- hashing_01.py
+++ hashing_01.py
@@ -16,18 +16,18 @@
 
 # **************************************************
 
-m = [1,2,3,3,4,3,9,10,1,0,7]
-n = [7,69,2,0,11,3,3,1,10]
-hash_list = [0] * 11
-for num in n:
-    if 0 <= num < len(hash_list):
-        hash_list[num] += 1
-# print(hash_list)
+# m = [1,2,3,3,4,3,9,10,1,0,7]
+# n = [7,69,2,0,11,3,3,1,10]
+# hash_list = [0] * 11
+# for num in n:
+#     if 0 <= num < len(hash_list):
+#         hash_list[num] += 1
+# # print(hash_list)
 
-for num in m:
-    if num<1 or num>10:
-        print(0)
-    print(hash_list[num])
+# for num in m:
+#     if num<1 or num>10:
+#         print(0)
+#     print(hash_list[num])
 
 
     # time_complexity = O(n+m)
@@ -36,3 +36,27 @@ for num in m:
     # for this approach, we are using a fixed-size list (hash_list) to store the counts of elements from n. This means that the space used by hash_list does not grow with the size of the input lists m and n, but is instead constant (O(1)) because its size is determined by the range of possible values (0-10 in this case).
         
     # time complexity still remains O(n + m) because we still need to iterate through both lists once.
+#**************************************************
+
+# using hashing we can solve it 
+
+n = [7,69,2,0,11,3,3,1,10]
+m = [1,2,3,3,4,3,9,10,1,0,7]
+
+hash_map = {}
+for num in n:
+    hash_map[num] = hash_map.get(num, 0) + 1
+
+for num in m:
+    print(hash_map.get(num, 0))
+
+
+
+#charecter_hashing:
+s = "axhdisgrhdia"
+q = ["d", "a", "x", "z", "h", "i"]
+char_map = {}
+for char in s:
+    char_map[char] = char_map.get(char, 0) + 1
+for char in q:
+    print(char_map.get(char, 0))
