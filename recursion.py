69cd118145ef48637211f2fea500a140cc92ed96
shubhapramanik07 <shubhapramanik07@gmail.com>
2025-09-08 16:08:17 +0530

    recursion.

--- recursion.py
+++ recursion.py
@@ -6,29 +6,73 @@
 
 
 
-
-# from itertools import count
-def func(count):
-    # Head recursion
-    if count == 4:
-        return
-    print("shubha")
-    func(count + 1)
+#******************************************
 
 
-def func_tail(count):
-    # Tail recursion
-    if count == 4:
-        return
-    func_tail(count + 1)
-    print("pramanik")
+# # from itertools import count
+# def func(count):
+#     # Head recursion
+#     if count == 4:
+#         return
+#     print("shubha")
+#     func(count + 1)
 
 
-# Run both
-print("Head Recursion Output:")
-func(0)
+# def func_tail(count):
+#     # Tail recursion
+#     if count == 4:
+#         return
+#     func_tail(count + 1)
+#     print("pramanik")
 
-print("\nTail Recursion Output:")
-func_tail(0)
 
-# i want to push it into github
+# # Run both
+# print("Head Recursion Output:")
+# func(0)
+
+# print("\nTail Recursion Output:")
+# func_tail(0)
+
+#******************************************
+
+
+# time complexity of both is O(n)
+# space complexity of both is O(n) due to call stack
+
+
+#******************************************
+
+# recursion using parameters............\\
+
+# def func(x,n):
+#     if n == 0:
+#         return
+#     func(x,n-1)
+#     print(x)
+
+# func(45,5)  
+
+
+#******************************************
+# Q) sum of 1 to n natural numbers....
+
+# def sum(n,add):
+#     if n < 0:
+#         return(print(add))
+#     add += n
+#     sum(n-1,add)
+
+
+# add = 0
+# sum(3,add)
+#******************************************
+
+def func(sum,i,N):
+    if i<=N:
+        sum += i
+        return func(sum,i+1,N)
+    return sum
+
+
+x = func(0,0,5)
+print(x)
