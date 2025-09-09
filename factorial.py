25dd5e6fcc12a482dba55ac8fd40bad4d14e44a3
shubhapramanik07 <shubhapramanik07@gmail.com>
2025-09-09 11:34:19 +0530

    recursion.

---
+++ factorial.py
@@ -0,0 +1,14 @@
+# factorial_of_a_number...........\\
+
+# def fact(num):
+#     if num==1 or num==0:
+#         return 1
+
+#     if num>0:
+#         return num * fact(num-1)
+
+# x = fact(5)
+# print(x)
+
+# space complexity is O(n) due to call stack
+# time complexity is O(n)
